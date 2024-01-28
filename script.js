62400070df8898299437969042411ac5434341ff
Mark <markanasco55@gmail.com>
2024-01-28 11:53:30 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector(".hamburger");
+const sidebar = document.querySelector(".sidebar");
+const closeSidebar = document.querySelector(".close-sidebar");
+
+hamburger.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+});
+
+closeSidebar.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+});
